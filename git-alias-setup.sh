c88841a46b055075c39d3785094d6c7872041915
gauravsingh94 <gauravrajputsingh94@gmail.com>
2023-10-14 17:05:18 +0530

    added a script for setting up the git alias

--- git-alias-setup.sh
+++ git-alias-setup.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
-
 # Set Git aliases
 git config --global alias.gc commit
 git config --global alias.gs status
 git config --global alias.ga add
-git config --global alias.gps push
+git config --global alias.gpss push  # Corrected alias name
 git config --global alias.gpl pull
 git config --global alias.gr reset
 
 echo "Git aliases are configured:"
 echo "gc -> commit"
-echo "gst -> status"
+echo "gs -> status"
 echo "ga -> add"
 echo "gps -> push"
 echo "gpl -> pull"
